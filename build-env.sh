bc8f2481ff7c198582710ae72d5cf0e165df3eb3
barkar19 <barkar19@gmail.com>
2017-11-22 14:40:00 +0100

    Ubuntu based docker

--- build-env.sh
+++ build-env.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+trap "exit" INT
+if [ -z $1 ]; then
+	echo "Choose build type: arch or ubuntu"
+	exit
+fi
+
 #Download wosedon
 wget https://clarin-pl.eu/dspace/bitstream/handle/11321/290/wosedon.7z
 #unpack
@@ -8,10 +14,10 @@ wget https://clarin-pl.eu/dspace/bitstream/handle/11321/290/wosedon.7z
 systemctl start docker
 
 #build from Dockerfile
-docker build -t wosedon .
+docker build -t wosedon-$1 -f Dockerfile-$1 .
 
 #run with 
-docker run -a stdin -a stdout -i -t wosedon /bin/bash
+docker run -a stdin -a stdout -i -t wosedon-$1 /bin/bash
 
 
 
